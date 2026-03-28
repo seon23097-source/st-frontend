a05ec4b0a9ae8d85d3240fa90874cb9a1d7aaa36
seon23097-source <seon23097@gmail.com>
2026-03-28 10:40:09 +0900

    자리배치 기능 추가2

--- src/components/SeatingArrangement.jsx
+++ src/components/SeatingArrangement.jsx
@@ -161,7 +161,7 @@ function SeatingArrangement() {
             }
           }
         });
-      });
+      }
     }
     return { sorted, adjacentPairs };
   }, [noisyStudents, grid, isNoisyStudent]);
